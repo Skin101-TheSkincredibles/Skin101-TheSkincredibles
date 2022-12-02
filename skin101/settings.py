4c73216995215e027bd710ed5cd8947c42b02283
Agnes Audya Tiara P <agnes.audya@ui.ac.id>
2022-12-02 16:41:57 +0700

    add datatags.json as initial data

--- skin101/settings.py
+++ skin101/settings.py
@@ -122,6 +122,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# FIXTURE_DIRS = (
+#    os.path.join(BASE_DIR, 'fixtures')
+# )
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
