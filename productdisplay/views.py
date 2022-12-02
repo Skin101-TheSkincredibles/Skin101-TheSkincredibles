3147063f63d5433503a9d33f922a1661ce1bcbfb
Agnes Audya Tiara P <agnes.audya@ui.ac.id>
2022-12-02 22:02:29 +0700

    add get_products and filter_by(tags) methods

--- productdisplay/views.py
+++ productdisplay/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from productdisplay.models import Tags, SkinCareItem
+import sqlite3
 # Create your views here.
+
+def get_products(): #get all products
+    return SkinCareItem.objects.all()
+
+def filter_by(tags):
+    all_products = get_products()
+    return all_products.filter(tags__pk__in=tags)
+
+def index(request):
+    #TES aja dibawah
+    res = filter_by([2,4]).distinct()
+    print(res)
+    return HttpResponse("Hello, world. You're at the polls index.")
